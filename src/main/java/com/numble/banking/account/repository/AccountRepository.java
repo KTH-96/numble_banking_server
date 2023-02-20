74952c13ddc134f2bc69d2a687e7a455d1b5f5ad
KTH-96 <taehyun960@gmail.com>
2023-02-20 19:42:47 +0900

    feat: 내 계좌 반환 로직 설계

--- src/main/java/com/numble/banking/account/repository/AccountRepository.java
+++ src/main/java/com/numble/banking/account/repository/AccountRepository.java
@@ -2,9 +2,16 @@ package com.numble.banking.account.repository;
 
 import com.numble.banking.account.Account;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
 	Optional<Boolean> existsByNumber(String accountNumber);
+
+	@Query("select a from Account a join a.member where a.member.id = :member_id")
+	Page<Account> findAccountById(@Param("member_id") Long id, Pageable pageable);
 }
--- src/main/java/com/numble/banking/account/service/AccountService.java
+++ src/main/java/com/numble/banking/account/service/AccountService.java
@@ -1,8 +1,13 @@
 package com.numble.banking.account.service;
 
 import com.numble.banking.account.Account;
+import com.numble.banking.account.dto.response.MyAccountResponse;
+import com.numble.banking.member.dto.LoginMember;
+import org.springframework.data.domain.Pageable;
 
 public interface AccountService {
 
 	Account createAccount();
+
+	MyAccountResponse findMyAccounts(LoginMember loginMember, Pageable pageable);
 }
