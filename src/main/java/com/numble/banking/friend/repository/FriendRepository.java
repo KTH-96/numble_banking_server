99fe91b21cabbb4c9eafe16b182460d33895d393
KTH-96 <taehyun960@gmail.com>
2023-02-21 15:54:54 +0900

    feat: 친구 repository생성

---
+++ src/main/java/com/numble/banking/friend/repository/FriendRepository.java
@@ -0,0 +1,8 @@
+package com.numble.banking.friend.repository;
+
+import com.numble.banking.friend.Friend;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+
+}
