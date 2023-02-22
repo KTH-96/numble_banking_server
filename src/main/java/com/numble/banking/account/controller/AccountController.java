308aff68afc05ec432ca7e3130b82ad5b3ac7cef
KTH-96 <taehyun960@gmail.com>
2023-02-22 19:21:32 +0900

    feat: 계좌이체 API 작성

--- src/main/java/com/numble/banking/account/controller/AccountController.java
+++ src/main/java/com/numble/banking/account/controller/AccountController.java
@@ -1,6 +1,8 @@
 package com.numble.banking.account.controller;
 
+import com.numble.banking.account.dto.request.AccountTransferRequest;
 import com.numble.banking.account.dto.response.AccountResponse;
+import com.numble.banking.account.dto.response.AccountTransferResponse;
 import com.numble.banking.account.service.AccountService;
 import com.numble.banking.member.dto.LoginMember;
 import com.numble.banking.common.utils.Login;
@@ -11,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +32,11 @@ public class AccountController {
 		return accountService.findMyAccounts(loginMember, pageable);
 	}
 
-
+	@PostMapping("/transfer")
+	private AccountTransferResponse transfer(
+		@Login @Valid LoginMember loginMember,
+		@RequestBody @Valid AccountTransferRequest request
+	) {
+		return accountService.transferMoney(loginMember, request);
+	}
 }
