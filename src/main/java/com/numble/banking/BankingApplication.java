8a99566c770abcba54adbbb476f84eb4dd20aca7
KTH-96 <taehyun960@gmail.com>
2023-03-17 20:25:53 +0900

    feat: 회원가입시 가입시간과 수정시간에 대한 컬럼 추가

--- src/main/java/com/numble/banking/BankingApplication.java
+++ src/main/java/com/numble/banking/BankingApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @ConfigurationPropertiesScan
 @EnableFeignClients
+@EnableJpaAuditing
 @SpringBootApplication
 public class BankingApplication {
 
